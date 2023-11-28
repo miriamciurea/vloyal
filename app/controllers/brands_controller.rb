ac6dae560204f9000d12250420482827c761bd29
anup ale <anoop.aale07@gmail.com>
2023-11-28 17:47:33 +0000

    few works on the brand controller and show page

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,6 +1,6 @@
 class BrandsController < ApplicationController
   def show
     @brand = Brand.find(params[:id])
-     @location = Location.find(params[:id])
+    @location = Location.find(params[:id])
   end
 end
