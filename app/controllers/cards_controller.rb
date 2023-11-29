ef6645bc24447e85b11d215254a0c3ce5b57e150
Miriam Ciurea <miriamciurea777@icloud.com>
2023-11-29 18:04:54 +0000

    functionality is there

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,4 +3,24 @@ class CardsController < ApplicationController
     @cards = current_user.cards
     @page_cards = true
   end
+
+  # def new
+  #   @card = Card.new
+  # end
+
+  def create
+    @brand = Brand.find(params[:brand_id])
+    @card = Card.new
+    @card.brand = @brand
+    @card.user = current_user
+    @card.save
+    redirect_to brands_path, notice: "card created successfully!"
+  end
+
+private
+
+# def card_params
+#   params.require(:card).permit(:stamps, :brand_id, :brand_id, :user_id)
+# end
+
 end
