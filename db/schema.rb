f697d992b671cb49a85edca9cb7e99b4798ee325
Miriam Ciurea <miriamciurea777@icloud.com>
2023-12-01 16:39:39 +0000

    master fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2023_11_30_172040) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
