b9c0974fb616b283de3b9f489bcf233a207a4827
Miriam Ciurea <miriamciurea777@icloud.com>
2023-12-04 15:13:24 +0000

    done

--- app/javascript/controllers/qrprompt_controller.js
+++ app/javascript/controllers/qrprompt_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="prompt"
 export default class extends Controller {
 
-  static targets = ["prompt"]
+  static targets = ["prompt", 'img', 'card']
 
   connect() {
     console.log('Hello World')
@@ -11,9 +11,29 @@ export default class extends Controller {
 
   showPrompt(e) {
     e.preventDefault()
+    let targetCard;
+    console.log(e);
+    console.log(this.cardTargets[0]);
+    this.cardTargets.forEach(card => {
+      if (card.contains(e.target)) {
+        targetCard = card
+      }
+    });
+    this.imgTarget.src = targetCard.dataset.src
+
+    console.log(e);
     this.promptTarget.classList.remove("d-none")
     setTimeout(() => {
       this.promptTarget.classList.add("show")
     }, 10);
+  }
+
+  hidePrompt(e) {
+    console.log("hide prompt");
+    e.preventDefault(e)
+    this.promptTarget.classList.remove("show")
+    setTimeout(() => {
+      this.promptTarget.classList.add("d-none")
+    }, 300);
   }
 }
