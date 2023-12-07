bc4cb3880ae993c04867fd833e477ab600bddebf
anup ale <anoop.aale07@gmail.com>
2023-12-07 11:37:37 +0000

    still working on the show and map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -17,8 +17,10 @@ export default class extends Controller {
     });
     this.#addMarkerToMap();
     this.#fitMapToMarker();
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }))
+    this.map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+    }))
   }
 
   #addMarkerToMap() {
@@ -59,6 +61,6 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
 
     this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 13, duration: 2000 })
+    this.map.fitBounds(bounds, { padding: 124, maxZoom: 15, duration: 2000 })
   }
 }
